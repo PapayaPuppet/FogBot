118052c7a55a3c4b9a2e16636a59b2da99555fe3
PapayaPuppet <ethan.p.shook.us@gmail.com>
2024-12-19 15:36:13 -0500

    Updated env import to use process.cwd()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
-import path from 'path'
 import dotenv from 'dotenv'
-dotenv.config({ path: path.join(__dirname, "../.env")})
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 const client: SapphireClient = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
